ba30c4da44fa95e43f39900cda6fcf4b0d67d024
Gabriel <gabriel.ramirez.lemus@gmail.com>
2020-08-20 02:23:48 -0400

    Se utilizó js para bootstrap list-group

--- public/index.js
+++ public/index.js
@@ -1,3 +1,15 @@
 //estructura mensajes exportados 
 // dd/mm/aa hh:mm p./a. m. - Nombre usuario: mensaje
 
+const setActive = () => {
+    let element, name, array;
+    element = document.getElementsById("optn");
+    name = "active";
+    array = element.className.split(" ");
+    if (array.indexOf(name) == -1) {
+        element.className += " " + name;
+    }
+}
+
+
+
